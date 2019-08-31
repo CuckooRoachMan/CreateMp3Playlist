95102946fa6f2d0796603c3075d6fd4630a420fc
CuckooRoachMan <31759040+CuckooRoachMan@users.noreply.github.com>
2019-08-31 03:50:25 -0600

    Update createPlaylist.sh

--- createPlaylist.sh
+++ createPlaylist.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/bash
-for dir in /run/media/jamz/DRAGON/Music/*/*/
+for dir in /Path/to/Music/*/*/
 do
-    #echo "${dir}"
-    #cd "${dir}"
     for file in "${dir}"/* ; 	    
     do
 	echo "${file}"
 	if  [ ${file: -4} == ".mp3" ]
 	then 
-	    realpath "$file" >> /home/jamz/playlist.txt;
+	    realpath "$file" >> playlist.txt;
 	    echo "$file";
 	fi   
     done
